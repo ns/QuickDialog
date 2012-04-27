dcb49672b9dad3958d698489b5bd63f645964dcd
Nitin Shantharam <atomic.unit@gmail.com>
2012-04-27 01:29:51 -0700

    check for NSNull before NSObject to prevent crashes

--- quickdialog/QRootBuilder.m
+++ quickdialog/QRootBuilder.m
@@ -36,6 +36,8 @@ NSDictionary *QRootBuilderStringToTypeConversionDict;
         [target setValue:value forKeyPath:propertyName];
     } else if ([value isKindOfClass:[NSDictionary class]]){
         [target setValue:value forKeyPath:propertyName];
+    } else if ([[NSNull null] isEqual:value]){
+        [target setValue:nil forKeyPath:propertyName];
     } else if ([value isKindOfClass:[NSObject class]]){
         [target setValue:value forKeyPath:propertyName];
     } else if (value == nil){
